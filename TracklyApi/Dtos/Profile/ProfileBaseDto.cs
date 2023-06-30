5c8ad4458e194f2376b717a71b6a8606928bda19
qbasic13 <qbasic13@outlook.com>
2023-06-30 15:19:19 +0300

    Fix ProfileBaseDto strings declared as nullable

--- TracklyApi/Dtos/Profile/ProfileBaseDto.cs
+++ TracklyApi/Dtos/Profile/ProfileBaseDto.cs
@@ -2,6 +2,6 @@ namespace TracklyApi.Dtos.Profile;
 public class ProfileBaseDto
 {
     public string FirstName { get; set; } = null!;
-    public string? LastName { get; set; } = null!;
-    public string? MiddleName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string MiddleName { get; set; } = null!;
 }
